7c4deebc0e62cb4c88cf18db972072f022614970
Mikhael Pedro <mikhaelpedro2000@gmail.com>
2023-03-21 23:28:08 -0300

    Exemplo Finalizado

--- WebFMethod/Default.aspx.cs
+++ WebFMethod/Default.aspx.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,6 +15,26 @@ namespace WebFMethod
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+        [System.Web.Services.WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public static string MeuMetodo()
+        {
+            // Cria um objeto para ser retornado como JSON
+            var obj = new
+            {
+                nome = "Exemplo",
+                idade = 30
+            };
+
+            // Converte o objeto para JSON e retorna
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        [WebMethod]
+        public static string MeuMetodo2()
+        {
+            return "Olá, mundo!";
         }
     }
 }
